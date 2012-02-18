77ba8800e0b7c550c43acc797e89b988a735ce62
Daniel Ward <dwa012@gmail.com>
2012-02-18 17:49:01 -0600

    Auto Commit: Sat Feb 18 17:49:01 CST 2012

--- priority_queue.c
+++ priority_queue.c
@@ -1,10 +1,12 @@
 #include "priority_queue.h"
 #include <stdio.h>
 
-#define QUEUE_FULL -1
-#define QUEUE_TOKEN_ALREADY_IN_USE -2
-#define QUEUE_PRIORITY_INVALID -3
+#define MAXIMUM_NUMBER_OF_QUEUES 				1024
+#define MAXIMUM_NUMBER_OF_ELEMENTS_IN_A_QUEUE	1024
+#define QUEUE_FULL 								-1
+#define QUEUE_TOKEN_ALREADY_IN_USE 				-2
+#define QUEUE_PRIORITY_INVALID 					-3
 
 QUEUE_TICKET create(){
-	printf("size f %u\n",sizeof(long));
+	printf("size f %d \n",QUEUE_FULL);
 }
