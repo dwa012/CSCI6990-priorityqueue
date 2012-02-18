032104d385c50b2658785336f395ec3b8cffee6b
Daniel Ward <dwa012@gmail.com>
2012-02-18 11:38:36 -0600

    Auto Commit: Sat Feb 18 11:38:36 CST 2012

---
+++ priority_queue.c
@@ -0,0 +1,16 @@
+#include "priority_queue.h"
+
+struct queue{
+	int * queue;
+	int size;
+	int head;
+	int max_size;
+}
+
+queue **queues;
+int number
+
+int token_to_index(int token){
+	return 0;
+}
+
