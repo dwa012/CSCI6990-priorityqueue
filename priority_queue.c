0e95c0cb0bda5f8d81137cf67ced11a9f9baa528
Daniel Ward <dwa012@gmail.com>
2012-02-18 17:43:01 -0600

    Auto Commit: Sat Feb 18 17:43:01 CST 2012

--- priority_queue.c
+++ priority_queue.c
@@ -1,6 +1,10 @@
 #include "priority_queue.h"
 #include <stdio.h>
 
+#define QUEUE_FULL
+#define QUEUE_TOKEN_ALREADY_IN_USE
+#define 
+
 QUEUE_TICKET create(){
 	printf("size f %u\n",sizeof(long));
 }
